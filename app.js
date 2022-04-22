60f13ed1777de02d8ffeabd3dee4a7647dc3a60f
Dana <danamudrova@gmail.com>
2022-04-22 20:45:19 +0200

    [FEATURE] added dataset into all created recipes, added event listener onclick to all created recipes

--- app.js
+++ app.js
@@ -14,34 +14,56 @@ function recipe(index) {
     document.querySelector('.recepty').appendChild(viewPicture);
 
     let viewTitle = document.createElement('div');
-    viewTitle.className = 'recept-info'
+    viewTitle.className = 'recept-info';
     let title = document.createElement('h3');
     title.innerHTML = recepty[index].nadpis;
     viewTitle.appendChild(title);
     document.querySelector('.recepty').appendChild(viewTitle);
 
     let createRecipe = document.createElement('div');
-    createRecipe.className = 'recept'
-    createRecipe.appendChild(viewPicture)
-    createRecipe.appendChild(viewTitle)
+    createRecipe.className = 'recept';
+    createRecipe.setAttribute('data-vyber',[index]);
+    //přidá každému vytvořenému receptu klikatelnost, která spustí funkci zobrazení detailu
+    createRecipe.onclick = changeRecipeDetails;
+
+    createRecipe.appendChild(viewPicture);
+    createRecipe.appendChild(viewTitle);
     document.querySelector('.recepty').appendChild(createRecipe);
 }
 
 // funkce pro vygenerování seznamu receptů z databáze do prvku id="recepty"
 function showAllRecipe(){
     for(let i = 0; i < recepty.length; i++) {
-		recipe(i);
-    }
+		recipe(i); 
+        };
 }
 
-/*
+
+/* 
+
+filtrováí polí 
+
+
 2) Doplň hledání - v hlavičce odkomentuj pole pro hledání. Pri kliknutí na tlačítko Hledat
 by se měl seznam receptů vyfiltrovat podle hledaného slova.
 
 3) Doplň filtrovanání receptů podle kategorie.
 
-4) Doplň řazení receptů podle hodnocení.
+4) Doplň řazení receptů podle hodnocení.*/
 
+
+//funkce měnící detail receptu podle toho, na co se kliklo 
+function changeRecipeDetails(){
+    
+    // document.querySelector('#recept-foto').src= recepty[myChoice].img;
+    // document.querySelector('#recept-kategorie').innerHTML= recepty[index].kategorie;
+    // document.querySelector('#recept-hodnoceni').innerHTML= recepty[index].hodnoceni;
+    // document.querySelector('#recept-nazev').innerHTML= recepty[index].nadpis;
+    // document.querySelector('#recept-popis').innerHTML= recepty[index].popis;
+}
+
+
+/*
 5) Na recepty v seznamu by mělo jít kliknout a na pravé polovině, se objeví detail receptu.
 Doplň patričné údaje receptu do HTML prvků s ID recept-foto, recept-kategorie,
 recept-hodnoceni, recept-nazev, recept-popis.
